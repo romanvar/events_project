50f227ad44d681dbcc869f2a08fcea3c3a0b16fe
romanvar <roman@varadi.cz>
2023-06-15 17:55:13 +0200

    User Entities commit

---
+++ src/main/java/cz/varadi/events_project/entities/RoleEntity.java
@@ -0,0 +1,25 @@
+package cz.varadi.events_project.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name="roles")
+@Setter
+@Getter
+
+public class RoleEntity {
+    @Id()
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="id")
+    public long id;
+
+    @Column(name="name", unique = true, nullable = false)
+    public String name;
+
+    public RoleEntity(String name) {
+        this.name = name;
+    }
+}
