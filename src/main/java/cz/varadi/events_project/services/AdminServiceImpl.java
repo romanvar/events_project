43699cc3382b3a506b70206492f082e14010e1f5
romanvar <roman@varadi.cz>
2023-06-29 19:30:18 +0200

    Admin Controller - first draft

---
+++ src/main/java/cz/varadi/events_project/services/AdminServiceImpl.java
@@ -0,0 +1,11 @@
+package cz.varadi.events_project.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminServiceImpl implements AdminService{
+    @Override
+    public void getUserList() {
+
+    }
+}
