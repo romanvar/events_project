ba771f965b965450a4f65cb4d0374b47d22f1f24
romanvar <roman@varadi.cz>
2023-07-04 19:08:02 +0200

    Access to admin.html added

--- src/main/java/cz/varadi/events_project/configurations/WebSecurityConfig.java
+++ src/main/java/cz/varadi/events_project/configurations/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,6 +16,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
 
     CustomUserDetailsService userDetailsService;
@@ -28,11 +30,13 @@ public class WebSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/login", "/user/registration", "/css/**", "/js/**")
                         .permitAll()
-                        .requestMatchers("/dashboard", "/events","/admin")
+                        .requestMatchers("/dashboard", "/events")
                         .authenticated()
+                        .requestMatchers(",/admin").hasRole("ADMIN")
 
                 ).formLogin(form -> form
                         .loginPage("/login")
---
+++ src/main/java/cz/varadi/events_project/exceptions/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package cz.varadi.events_project.exceptions;
+
+public class UserNotFoundException extends Exception {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
