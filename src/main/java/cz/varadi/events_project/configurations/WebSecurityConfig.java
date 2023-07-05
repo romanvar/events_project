d965ce5c20cbad764712db707a91b79f282c9cee
romanvar <roman@varadi.cz>
2023-07-05 17:36:34 +0200

    Access to admin.html added - admin acces corrected

--- src/main/java/cz/varadi/events_project/configurations/WebSecurityConfig.java
+++ src/main/java/cz/varadi/events_project/configurations/WebSecurityConfig.java
@@ -36,7 +36,7 @@ public class WebSecurityConfig {
                         .permitAll()
                         .requestMatchers("/dashboard", "/events")
                         .authenticated()
-                        .requestMatchers(",/admin").hasRole("ADMIN")
+                        .requestMatchers("/admin").hasRole("ADMIN")
 
                 ).formLogin(form -> form
                         .loginPage("/login")
